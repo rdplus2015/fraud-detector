0f62a1c0da53ea11558b516a68851163c1952fb1
Ridi Otoko <rdplus2015@gmail.com>
2026-08-22 22:52:04 -0400

    vite config added, app.jsx routing added

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/navbar.tsx";
+import {TransactionForm} from "./components/transactionForm.jsx";
+import Profile from "./components/profile.jsx";
+import History from "./components/history.jsx";
 
-function App() {
-  return (
-      <>
-        <h1 className="text-3xl font-bold underline bg-red-800">
-          Hello world!
-        </h1>
-      </>
-  )
+export default function App() {
+    return (
+        <BrowserRouter>
+            <div className="min-h-screen bg-base-200/40">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<TransactionForm />} />
+                    <Route path="/history" element={<History />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
-
-export default App
